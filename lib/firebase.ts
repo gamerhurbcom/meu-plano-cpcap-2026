ba35c5bae51dad0fd37d78470bfccbb29c4e95c0
gamerhurbcom <gamerhurb.com@gmail.com>
2026-07-06 21:33:00 -0300

    Adiciona sincronização com Firebase Firestore

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,21 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyByNf4uv7IkWw6Wb9nnQshxeGnM2oH0Kio",
-  authDomain: "cap2026-74371.firebaseapp.com",
-  projectId: "cap2026-74371",
-  storageBucket: "cap2026-74371.firebasestorage.app",
-  messagingSenderId: "320511555252",
-  appId: "1:320511555252:web:5bd9c1457c1ab073e689e1",
-  measurementId: "G-QWRQB9JXYJ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+console.log("Firebase config:", firebaseConfig);
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
